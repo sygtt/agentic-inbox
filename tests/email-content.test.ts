a7e166291b427db5f2a839a6dc74bb0ed8e7f10b
Jackoforever <sygttjako@gmail.com>
2026-08-27 01:33:17 +0900

    fix: respect implicit HTML head boundaries

--- tests/email-content.test.ts
+++ tests/email-content.test.ts
@@ -27,6 +27,7 @@ test("normalizes HTML fragments and decodes HTML entities", () => {
 	assert.equal(stripHtmlToText("<p><span>Hello</span>, world</p>"), "Hello, world");
 	assert.equal(stripHtmlToText("<p>inter<wbr>national <font>mail</font><nobr>box</nobr></p>"), "international mailbox");
 	assert.equal(stripHtmlToText("<head><title>Account alert</title></head><body>Actual message</body>"), "Actual message");
+	assert.equal(stripHtmlToText("<head><title>Alert</title><body>Actual message</body>"), "Actual message");
 	assert.equal(stripHtmlToText("<center>First</center><center>Second</center>"), "First Second");
 });
 
