6bf5565d9752d810e7aba90dce37d90e857cd4d1
sygtt <79931779+sygtt@users.noreply.github.com>
2026-08-31 03:36:40 +0900

    fix: mark page language as Japanese

--- app/root.tsx
+++ app/root.tsx
@@ -77,7 +77,7 @@ const KumoLink = forwardRef<
 
 export function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
+		<html lang="ja">
 			<head>
 				<meta charSet="UTF-8" />
 				<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
